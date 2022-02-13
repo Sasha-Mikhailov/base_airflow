16aee918ab5c39d64b58889b7aa907021208eda9
Sasha Mikhailov <sasha@sashamikhailov.ru>
2022-02-13 17:32:23 +0300

    tuning index

--- dags/common/meta.py
+++ dags/common/meta.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Table, Column, MetaData, Numeric, Date, String, DateTime
+from sqlalchemy import Table, Column, Index, MetaData, Numeric, Date, String, DateTime
 from sqlalchemy.engine import Engine
 
 
@@ -8,11 +8,14 @@ rates = Table(
     'exchange_rates', MetaData(),
     Column('currency_from', String()),
     Column('currency_to', String()),
-    Column('date', Date(), index=True),  # create index to optimise idempotency deletions
+    Column('date', Date()),
     Column('rate', Numeric(15, 6)),
     Column('utc_created_dttm', DateTime),
     Column('utc_updated_dttm', DateTime, default=datetime.utcnow()),
 )
+
+# create index to optimise idempotency deletions
+Index('rates_date_descending', rates.c.date.desc())
 
 
 def create_table_if_not_exists(engine: Engine, table: Table):
